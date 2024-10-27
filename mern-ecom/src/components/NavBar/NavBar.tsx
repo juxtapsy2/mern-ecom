4f085b7417ddf8fc487370bee570e1da245e36f5
juxtapsy2 <frost.death.ap@gmail.com>
2024-10-27 19:45:41 +0700

    Update tsconfig

--- mern-ecom/src/components/NavBar/NavBar.tsx
+++ mern-ecom/src/components/NavBar/NavBar.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from 'react';
 import {Row, Col, Input} from 'antd';
 import {NavWrapper, NavText} from './NavWrapper';
 import type { GetProps } from 'antd';
 
 const {Search} = Input;
 type SearchProps = GetProps<typeof Input.Search>;
-const onSearch: SearchProps['onSearch'] = (value, _e, info) => console.log(info?.source, value);
 
 export const NavBar = () => {  
   return (
     <div>
         <NavWrapper>
             <Col span={6}><NavText>Bill  1</NavText></Col>
-            <Search placeholder="input search text" onSearch={onSearch} enterButton />
+            <Search placeholder="input search text" enterButton />
             <Col span={12}><NavText>Bill  2</NavText></Col>
             <Col span={6}><NavText>Bill  3</NavText></Col>
         </NavWrapper>
